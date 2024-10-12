f00d0205eab37024a3c7bf3e1bc7c7d981062b97
Kokogino <gino-c@gmx.ch>
2024-10-12 18:00:29 +0200

    Add tags to image thumbnail in list.

--- ui/src/app/shared/components/image-thumbnail/image-thumbnail.component.ts
+++ ui/src/app/shared/components/image-thumbnail/image-thumbnail.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ImageResponse } from '@app/gen/ogm-backend';
+import { some } from 'lodash-es';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'ogm-image-thumbnail',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './image-thumbnail.component.html',
   styleUrl: './image-thumbnail.component.scss',
 })
@@ -17,5 +19,16 @@ export class ImageThumbnailComponent {
 
   onLoad(): void {
     this.loaded.emit(this.image.id);
+  }
+
+  hasTags(): boolean {
+    return some(this.image.tags, (tag) => tag.showTag);
+  }
+
+  getTags(): string {
+    return this.image.tags
+      .filter((tag) => tag.showTag)
+      .map((tag) => tag.name)
+      .join(', ');
   }
 }
